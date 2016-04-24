c29a261a372c335d8b5dac6a671492cc4e00f238
Nick Jones <tsip_67@yahoo.com>
2016-04-23 21:58:31 -0400

    Changed the header color to grey (for Jason) and changed the home link to index.php instead of index.html

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
 	
 	<!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-color:blue">
+    <header class="intro-header" style="background-color:grey">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
--- navigation.php
+++ navigation.php
@@ -16,7 +16,7 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
-                        <a href="index.html">Home</a>
+                        <a href="index.php">Home</a>
                     </li>
                     <li>
                         <a href="add_park.php">New Park</a>
